58d9d5001d0c8fa9a991dc2f578218e20a9e963f
colin <colin-bao@github.com>
2022-08-23 13:33:43 +0800

    update

--- div_cal/cal_div_v5.py
+++ div_cal/cal_div_v5.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 # ----------------参数和命名----------------#
-LAG_PERIOD = 5  # 滞后期
+LAG_PERIOD = 4  # 滞后期
 
 # ----------------读取原始数据----------------#
 DIV_TABLE = pd.read_parquet('AShareDividend.parquet')
@@ -43,7 +43,7 @@ TARGET_YEAR = (MV_INFO_TABLE['ann_date'].max() // 10000) - 1  # 参照期
 MV_INFO_TABLE.fillna(0, inplace=True)  # 没有merge到的缺失信息用0填充
 del MV_TABLE, INFO_TABLE, df_group  # 释放内存
 
-# ---------------测试数据---6007383在2018年3次分红------------#
+# ---------------测试数据---600738.SH在2018年有3次分红------------#
 MV_INFO_TABLE = MV_INFO_TABLE[MV_INFO_TABLE['stockcode'] == '600738.SH']
 
 ##################################################################
@@ -53,6 +53,7 @@ st = time.time()
 # ---------------重命名列名----------------#
 MV_INFO_TABLE.columns = [i[0] + '_' + str(+i[1]) if isinstance(i, tuple) else i for i in MV_INFO_TABLE.columns]
 
+# ---------------求出用于计算的基本矩阵:信息矩阵,年化因子矩阵,报告期矩阵,累积分红矩阵,分红因子激活矩阵,目标年份矩阵-----------------#
 for i in range(LAG_PERIOD):
     # ---------------可用信息矩阵----------------#
     MV_INFO_TABLE['info_{}'.format(i)] = np.where(MV_INFO_TABLE.eval('ann_date>ann_date_{i}'.format(i=i)), 1, 0)
@@ -74,7 +75,8 @@ for i in range(LAG_PERIOD):
 
 print('矩阵基础计算完成', time.time() - st)
 
-# ---------------在目标年份矩阵中迭代填充-得到最终的总分红----------------#
+# ---------------在目标输出矩阵中迭代填充-得到最终需要的值----------------#
+
 for i in range(LAG_PERIOD):
     right = LAG_PERIOD - 1 - i  # LAG_PERIOD 4: 0,1,2,3 ;right 3,2,1,0
     left = right - 1
@@ -114,21 +116,40 @@ for i in range(LAG_PERIOD):
             MV_INFO_TABLE['target_year_sum_ar_{}'.format(i)])
 
         del same_expr
-print('填充完成', time.time() - st)
+    # print('填充完成', time.time() - st)
 
-# ---------------求期望----------------#
-MV_INFO_TABLE.eval("""
-target_exp_year = ann_date//10000-1
-target_exp_year_pos=target_exp_year[(target_year_0-target_exp_year)<=@LAG_PERIOD]//1
-""", inplace=True)
-# np.where(MV_INFO_TABLE.eval('target_exp_year_pos'), MV_INFO_TABLE.eval(), MV_INFO_TABLE.eval())
+    # --------------填充当前交易日的T-1期的列----------------#
+    MV_INFO_TABLE.eval("""
+        target_year_t_{i} = ann_date//10000-1-{i}
+        target_flag_{i}= target_year_0-target_year_t_{i}
+        target_real_{i}=0
+        target_exp_ar_{i}=0
+        """.format(i=i), inplace=True)
+    # 更新需要的滞后年份
+    MV_INFO_TABLE['target_flag_{}'.format(i)] = np.where(MV_INFO_TABLE['target_flag_{}'.format(i)] > LAG_PERIOD, 0,
+                                                         MV_INFO_TABLE['target_flag_{}'.format(i)])
 
-# ---------------测试数据---------------#
-print('求期望完成', time.time() - st)
+    for j in range(LAG_PERIOD):
+        # ---------------填充-实际历史分红----------------#
+        MV_INFO_TABLE['target_real_{}'.format(i)] = np.where(MV_INFO_TABLE['target_flag_{}'.format(i)] == j,
+                                                             MV_INFO_TABLE['target_year_sum_{}'.format(j)],
+                                                             MV_INFO_TABLE['target_real_{}'.format(i)])
 
-MV_INFO_TABLE.sort_values(by='ann_date', ascending=False, inplace=True)
+        # ---------------填充-年化历史分红----------------#
+        MV_INFO_TABLE['target_exp_ar_{}'.format(i)] = np.where(MV_INFO_TABLE['target_flag_{}'.format(i)] == j,
+                                                               MV_INFO_TABLE['target_year_sum_ar_{}'.format(j)],
+                                                               MV_INFO_TABLE['target_exp_ar_{}'.format(i)])
+    # print('求滞后完成', time.time() - st)
+
+print('矩阵填充完成', time.time() - st)
+
 # ---------------输出目标数据---------------#
-MV_INFO_TABLE = MV_INFO_TABLE[['stockcode', 'ann_date'] + [i for i in MV_INFO_TABLE.columns if 'target' in i]]
+MV_INFO_TABLE.sort_values(by='ann_date', ascending=False, inplace=True)
+MV_INFO_TABLE = MV_INFO_TABLE[
+    ['stockcode', 'ann_date'] +
+    ['{}_{}'.format(i, j) for i in ['target_year_t', 'target_real', 'target_exp_ar'] for j in range(LAG_PERIOD)]]
+
 # MV_INFO_TABLE.to_csv('final.csv')
 # del MV_INFO_TABLE
+
 # MV_INFO_TABLE = MV_INFO_TABLE[MV_INFO_TABLE['ann_date'].astype('str').str[:-4].isin(['2017','2018', '2019'])]
